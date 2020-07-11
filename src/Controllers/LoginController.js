55767b4489d6a451b2ed76388bbb6504eb21696d
Jose Claudio <jjoclas@hotmail.com>
2020-07-11 19:02:36 -0300

    adicionado login empresa

--- src/Controllers/LoginController.js
+++ src/Controllers/LoginController.js
@@ -1,5 +1,6 @@
 var LoginModel = require('../Models/LoginModel');
 var Auth = require('../Utils/Auth');
+const { loginCompany } = require('../Models/LoginModel');
 
 var LoginController = {
 
@@ -8,7 +9,36 @@ var LoginController = {
         try{
             const {ds_email, ds_senha } = req.body;
 
-            const user = await LoginModel.doLogin(ds_email,ds_senha);
+            const user = await LoginModel.loginUser(ds_email,ds_senha);
+           
+            if(user != null){
+                var token = await  Auth.generateToken(user.id_usuario)
+
+                  return res.status(200).send({
+                    'userData': user,
+                    'auth': true,
+                    'token' : token
+                    });
+            }
+               
+            else{
+                return res.status(200).send({
+                    'message':'E-mail ou senha inválido!'
+                });
+            }
+        }
+        catch(err){
+            return res.status(500).send({
+                'error' : err
+            });
+        }
+    },
+    async loginCompany(req,res) {
+        
+        try{
+            const {ds_email, ds_senha } = req.body;
+
+            const user = await LoginModel.loginCompany(ds_email,ds_senha);
            
             if(user != null){
                 var token = await  Auth.generateToken(user.id_usuario)
--- src/Models/LoginModel.js
+++ src/Models/LoginModel.js
@@ -3,8 +3,8 @@ const knex = require('knex')(options);
 
 var LoginModel = {
 
-     async doLogin(ds_email,ds_senha){
-        console.log(ds_email)
+     async loginUser(ds_email,ds_senha){
+        
         try{
             var user = {};
 
@@ -26,7 +26,32 @@ var LoginModel = {
         catch(ex){
             throw ex;
         }
+    },
+     async loginCompany(ds_email,ds_senha){
+        
+        try{
+            var user = {};
+
+            await knex('tb_empresa')
+            .select()
+            .where({
+                ds_email: ds_email,
+                ds_senha: ds_senha
+            })
+            .then(function(res){
+                if(res.length >= 1)
+                    user = res;
+                else
+                    user = null;
+            });
+            
+            return user;
+        }
+        catch(ex){
+            throw ex;
+        }
     }
+    
 }
 
 module.exports = LoginModel;
