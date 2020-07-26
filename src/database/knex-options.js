b8005ef3802ba700de6e742df9cf30128ee29618
Jose Claudio <jjoclas@hotmail.com>
2020-07-26 19:18:15 -0300

    fixed connection

--- src/database/knex-options.js
+++ src/database/knex-options.js
@@ -1,11 +1,11 @@
 const options = {
     client: 'mysql',
     connection: {
-        host     : 'localhost',
-        port     : 3306,
-        user     : 'root',
-        password : 'rootqwer4321',
-        database : 'dt_stopcovid'
+        host     : 'mysql669.umbler.com',
+        port     : 41890,
+        user     : 'abraselstopcovid',
+        password : 'Abrasel2020',
+        database : 'abraselstopcovid'
     }
 }
 
