27bb6427ca5d1c1fd57b32da566a4d73f382b00f
Lucas Vilas Boas <lucasvilasboaslage@gmail.com>
2020-08-03 20:52:31 -0300

    Adicionando filtro

--- src/Models/FollowUpModel.js
+++ src/Models/FollowUpModel.js
@@ -42,30 +42,30 @@ var FollowUpModel = {
         }
         return valid;
     },
-        async getFollowUpDates(req, res){
-            try{
-                var users = {};
-                //const { id } = req.params
+    async getFollowUpDates(req, res){
+        try{
+            var users = {};
+            //const { id } = req.params
+        
+            await knex('tb_acompanhamento')
+            .select('tb_acompanhamento.id_usuario','dt_consulta','ds_nome','id_sintoma','ds_telefone', 'ds_usa_epi')
+            .leftJoin('tb_usuario','tb_acompanhamento.id_usuario','tb_usuario.id_usuario')
+            //.where('tb_acompanhamento.id_usuario', req.query.id_usario)
+            .whereBetween('dt_consulta',[req.query.data_inicio, req.query.data_final])
+            .orderByRaw(' dt_consulta desc, length(id_sintoma) desc')
+            .then(function(res){
+                if(res.length >= 1)
+                    users = res;
+                else
+                    users = null;
+            });
             
-                await knex('tb_acompanhamento')
-                .select('tb_acompanhamento.id_usuario','dt_consulta','ds_nome','id_sintoma','ds_telefone', 'ds_usa_epi')
-                .leftJoin('tb_usuario','tb_acompanhamento.id_usuario','tb_usuario.id_usuario')
-                .where('tb_acompanhamento.id_usuario', req.query.id_usario)
-                .whereBetween('dt_consulta',[req.query.data_inicio, req.query.data_final])
-                .orderByRaw(' dt_consulta desc, length(id_sintoma) desc')
-                .then(function(res){
-                    if(res.length >= 1)
-                        users = res;
-                    else
-                        users = null;
-                });
-                
-                return users;
-            }
-            catch(ex){
-                throw ex;
-            }
-        },
+            return users;
+        }
+        catch(ex){
+            throw ex;
+        }
+    }
 
 }
 module.exports = FollowUpModel;
